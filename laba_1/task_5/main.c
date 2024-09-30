926fcf1ff507f6bb0712b27d2b5f8aa6a9182b6d
Daniil Makhov <mahovdnl@gmail.com>
2024-10-01 01:28:41 +0300

    done task 1.5

--- laba_1/task_5/main.c
+++ laba_1/task_5/main.c
@@ -51,39 +51,56 @@ int getOpts(int argc, char** argv, double* epsilon, double* x){
 }
 
 double factorial(double n){
+    if (n == 0) return 1;
     double temp = 1;
     for(int i = 1; i <= n; i++) temp *= i;
     return temp;
 }
 
-double a_func(double n, double x){
-    return pow(x, n) / factorial(n);
+double double_factorial(double n) {
+    if (n == 0 || n == 1 || n == -1) return 1;
+    double temp = 1;
+    for (int x = n; x >= 1; x -= 2) temp *= x;
+    return temp;
 }
 
-double b_func(double n, double x){
-    return (pow(-1, n) * pow(x, 2*n) / factorial(2*n));
+
+double a_func(double n, double x, double prev_val){
+    if (prev_val == 0) return pow(x, n) / factorial(n);
+    // if (n == 0) return 1;
+    return prev_val * x/n;
+}
+
+double b_func(double n, double x, double prev_val){
+    if (prev_val == 0) return (pow(-1, n) * pow(x, 2*n) / factorial(2*n));
+    // if (n == 0) return 1;
+    return prev_val * (-1) * x*x / (2*n * (2*n - 1));
 }
 
 // насколько я понял ряды двух последних функций расходятся при x => 1
-double c_func(double n, double x){
-    return ((pow(3, 3*n) * pow(factorial(n), 3) * pow(x, 2*n)) / factorial(3*n));
+double c_func(double n, double x, double prev_val){
+    if (prev_val == 0) return ((pow(3, 3*n) * pow(factorial(n), 3) * pow(x, 2*n)) / factorial(3*n));
+    // if (n == 0) return 1;
+    return prev_val * ( (27 * x*x * n*n*n) / (27*n*n*n - 27*n*n + 6*n) );
 }
 
-double d_func(double n, double x){
-    return (pow(-1, n) * factorial(factorial(2*n - 1)) * pow(x, 2*n)) / factorial(factorial(2*n));
+double d_func(double n, double x, double prev_val){
+    if (prev_val == 0) return (pow(-1, n) * double_factorial(2*n - 1) * pow(x, 2*n)) / double_factorial(2*n);
+    // if(n == 1) return -(x * x)/2;
+    return prev_val * ( ((-1)*x*x*(2*n - 1)) / (2*n) );
 }
 
-double sum(double (*f)(double, double), double start_n, double eps, double x){
-    double prev_val = f(start_n, x);
+double sum(double (*f)(double, double, double), double start_n, double eps, double x){
+    double prev_val = f(start_n, x, 0);
     start_n++;
-    double val = prev_val + f(start_n, x);
+    double val = f(start_n, x, prev_val);
     double n = start_n;
-    while (fabs(val - prev_val) > eps){
-        prev_val = val;
+    while (fabs(val) > eps){
+        prev_val += val;
         n++;
-        val = prev_val + f(n, x);
+        val = f(n, x, val);
     }
-    return val;
+    return val + prev_val;
 }
 
 int main(int argc, char** argv){
@@ -105,15 +122,14 @@ int main(int argc, char** argv){
         }
     }
     
+    
+    int x_is_1 = (x >= 1) ? 1 : 0;
+
     printf("eps = %f, x = %f \n", eps, x);
     printf("\n");
-    printf("a = %f\n", sum(a_func, 0, eps, x));
-
-    printf("b = %f\n", sum(b_func, 0, eps, x));
-
-    printf("c = %f\n", sum(c_func, 0, eps, x));
-
-    printf("d = %f\n", sum(d_func, 1, eps, x));
-
+    printf("a: %f\n", sum(a_func, 0, eps, x));
+    printf("b: %f\n", sum(b_func, 0, eps, x));
+    printf(x_is_1 ? "c: числовой ряд расходится, x должен быть меньше 1\n" : "c: %f\n", sum(c_func, 0, eps, x));
+    printf(x_is_1 ? "d: числовой ряд расходится, x должен быть меньше 1\n" : "d: %f\n", sum(d_func, 1, eps, x));
     return 0;
 }
