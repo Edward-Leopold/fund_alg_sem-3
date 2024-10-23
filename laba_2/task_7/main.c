9dd227d156467565bca1671f19923788ddb47dd8
Daniil Makhov <mahovdnl@gmail.com>
2024-10-24 00:27:58 +0300

    remove annoying interval borders in printf funcs

--- laba_2/task_7/main.c
+++ laba_2/task_7/main.c
@@ -7,7 +7,7 @@ double ln2_equ(double x){
 }
 
 double exp_equ(double x){
-    return pow(x - 1.5, 2)/ (x - 1.5);
+    return pow(x - 1.5, 2) / (x - 1.5);
 }
 
 double dichotomy(double left, double right, double eps, double (*f)(double)){
@@ -28,17 +28,17 @@ int main(int argc, char** argv){
     double eps2 = 2;
     double eps3 = 0.00000000001;
 
-    printf("Equation 1 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 0.0, 1.0, eps1, dichotomy(0.0, 2.0, eps1, ln2_equ));
-    printf("Equation 1 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 2.0, 3.0, eps1, dichotomy(2.0, 3.0, eps1, ln2_equ));
-    printf("Equation 1 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 0.0, -1.0, eps1, dichotomy(0.0, -1.0, eps1, ln2_equ));
-    printf("Equation 1 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 0.0, 1.0, eps2, dichotomy(0.0, 1.0, eps2, ln2_equ));
-    printf("Equation 1 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 0.0, 1.0, eps3, dichotomy(0.0, 1.0, eps3, ln2_equ));
+    printf("Equation 1 for eps = %.10lf: %.10lf\n", eps1, dichotomy(0.0, 2.0, eps1, ln2_equ));
+    printf("Equation 1 for eps = %.10lf: %.10lf\n", eps1, dichotomy(2.0, 3.0, eps1, ln2_equ));
+    printf("Equation 1 for eps = %.10lf: %.10lf\n", eps1, dichotomy(0.0, -1.0, eps1, ln2_equ));
+    printf("Equation 1 for eps = %.10lf: %.10lf\n", eps2, dichotomy(0.0, 1.0, eps2, ln2_equ));
+    printf("Equation 1 for eps = %.10lf: %.10lf\n", eps3, dichotomy(0.0, 1.0, eps3, ln2_equ));
     printf("\n");
-    printf("Equation 2 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 0.0, 1.0, eps1, dichotomy(0.0, 2.0, eps1, exp_equ));
-    printf("Equation 2 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 2.0, 3.0, eps1, dichotomy(0.0, 2.1111, eps1, exp_equ));
-    printf("Equation 2 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 11.0, 12.0, eps1, dichotomy(11.0, 12.0, eps1, exp_equ));
-    printf("Equation 2 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 3.0, 5000.0, eps2, dichotomy(3.0, 50000.0, eps2, exp_equ));
-    printf("Equation 2 for [%lf, %lf] for eps = %.10lf: %.10lf\n", 5.0, 150.0, eps3, dichotomy(5.0, 150.0, eps3, exp_equ));
+    printf("Equation 2 for eps = %.10lf: %.10lf\n", eps1, dichotomy(0.0, 2.1111, eps1, exp_equ));
+    printf("Equation 2 for eps = %.10lf: %.10lf\n", eps1, dichotomy(11.0, 12.0, eps1, exp_equ));
+    printf("Equation 2 for eps = %.10lf: %.10lf\n", eps1, dichotomy(0.0, 2.0, eps1, exp_equ));
+    printf("Equation 2 for eps = %.10lf: %.10lf\n", eps2, dichotomy(3.0, 50000.0, eps2, exp_equ));
+    printf("Equation 2 for eps = %.10lf: %.10lf\n", eps3, dichotomy(5.0, 150.0, eps3, exp_equ));
 
     return 0;
 }
