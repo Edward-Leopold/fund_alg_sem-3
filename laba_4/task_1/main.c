20169deae5b920deb5925393c1f1624771ece87b
Daniil Makhov <mahovdnl@gmail.com>
2024-11-14 23:30:50 +0300

    getting args from cli

--- laba_4/task_1/main.c
+++ laba_4/task_1/main.c
@@ -6,6 +6,8 @@
 #include <linux/limits.h>
 
 typedef enum errCodes{
+    NOT_ENOUGH_ARGUEMENTS,
+    TOO_MANY_ARGUEMENTS,
     MALLOC_ERR,
     REALLOC_ERR,
     SAME_FILE_ERR,
@@ -30,11 +32,31 @@ errCodes is_same_file(const char *a, const char *b){
     return res;
 }
 
+errCodes getArgs(int argc, char** argv, char** filename){
+    if (argc < 2) return NOT_ENOUGH_ARGUEMENTS;
+    else if(argc > 2) return TOO_MANY_ARGUEMENTS;
 
+    *filename = argv[1];
+
+    return SUCCESS;
+}
 
 int main(int argc, char** argv){
-
-
+    char* filename = NULL;
+    errCodes args_status = getArgs(argc, argv, &filename);
+    if (args_status != SUCCESS){
+        switch (args_status){
+        case NOT_ENOUGH_ARGUEMENTS:
+            printf("Not enough number of argumnets.\n");
+            break;
+        case TOO_MANY_ARGUEMENTS:
+            printf("Too many argumnets.\n");
+            break;
+        default:
+            break;
+        }
+        return 1;
+    }
 
 
     return 0;
