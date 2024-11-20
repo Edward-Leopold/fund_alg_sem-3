be9a78f35f435da0bb521d0330fd69a600ce92e5
Daniil Makhov <mahovdnl@gmail.com>
2024-11-21 00:37:47 +0300

    done 5.4

--- laba_5/task_1/main.cpp
+++ laba_5/task_1/main.cpp
@@ -16,7 +16,6 @@ public:
     Complex(double c_real = 0, double c_imag = 0){
         real = c_real;
         imag = c_imag;
-        cout << "an object of complex object has been created: " << real << " + " << imag << "i" <<"\n";
     }
 
     Complex add(const Complex& other) const {
@@ -43,33 +42,69 @@ public:
         if (check_overflow(real_dif) || check_overflow(imag_sum)){
             throw overflow_error("Oveflow occured during operation");
         }
-        return Complex(
-            real_dif,
-            imag_sum
-        );
+        return Complex(real_dif, imag_sum);
     }
 
     Complex divide(const Complex& other) const {
         double denominator = other.real * other.real + other.imag * other.imag;
+        if (check_overflow(denominator)){
+            throw overflow_error("Oveflow occured during operation");
+        }
         if (denominator == 0) {
             throw invalid_argument("Division by zero in complex number");
         }
-        return Complex(
-            (real * other.real + imag * other.imag) / denominator,
-            (imag * other.real - real * other.imag) / denominator
-        );
+        double real_new = (real * other.real + imag * other.imag) / denominator;
+        double imag_new = (imag * other.real - real * other.imag) / denominator;
+        if (check_overflow(real_new) || check_overflow(imag_new)){
+            throw overflow_error("Oveflow occured during operation");
+        }
+        return Complex(real_new, imag_new);
     }
 
     void display() const{
         cout << real << " + " << imag << "i" << endl;
     }
+
+    double module() const { 
+        double val = real * real + imag * imag;
+        if (check_overflow(val)){
+            throw overflow_error("Oveflow occured during operation");
+        }
+        return sqrt(val); 
+    }
+
+    double argument() const { return atan2(imag, real); }
 };
 
 
 
 int main(){
-    Complex c;
-    
+    try{
+        Complex c1(3, 4);    
+        Complex c2(1, 2);   
+
+        Complex sum = c1.add(c2);
+        Complex diff = c1.subtract(c2); 
+        Complex prod = c1.multiply(c2);
+        Complex div = c1.divide(c2);  
+
+        cout << "Sum: ";
+        sum.display();
+        cout << "Difference: "; 
+        diff.display();
+        cout << "Product: "; 
+        prod.display();
+        cout << "Division: "; 
+        div.display();
+
+        double arg = c1.argument();
+        double mod = c1.module();
+
+        cout << "Arguement of c1: " << arg << endl;
+        cout << "Module of c1: " << mod << endl;
+    } catch(const std::exception& e){
+        cerr << e.what() << '\n';
+    }
 
     return 0;
 }
