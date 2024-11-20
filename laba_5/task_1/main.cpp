5a8075440ff1cf64cff183e244a7f90b0a5f0a0e
Daniil Makhov <mahovdnl@gmail.com>
2024-11-20 23:58:35 +0300

    add some overflow checks for 5.4

--- laba_5/task_1/main.cpp
+++ laba_5/task_1/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class Complex{
-    
-public:
+private:
     double real;
     double imag;
+
+    static bool check_overflow(const double num){
+        return (!isfinite(num) || isnan(num));
+    }
+public:
 
     Complex(double c_real = 0, double c_imag = 0){
         real = c_real;
@@ -15,17 +20,32 @@ public:
     }
 
     Complex add(const Complex& other) const {
-        return Complex(real + other.real, imag + other.imag);
+        double real_sum = real + other.real;
+        double imag_sum = imag + other.imag;
+        if (check_overflow(real_sum) || check_overflow(imag_sum)){
+            throw overflow_error("Oveflow occured during operation");
+        }
+        return Complex(real_sum, imag_sum);
     }
 
     Complex subtract(const Complex& other) const {
-        return Complex(real - other.real, imag - other.imag);
+        double real_sub = real - other.real;
+        double imag_sub = imag - other.imag;
+        if (check_overflow(real_sub) || check_overflow(imag_sub)){
+            throw overflow_error("Oveflow occured during operation");
+        }
+        return Complex(real_sub, imag_sub);
     }
 
     Complex multiply(const Complex& other) const {
+        double real_dif = real * other.real - imag * other.imag;
+        double imag_sum = real * other.imag + imag * other.real;
+        if (check_overflow(real_dif) || check_overflow(imag_sum)){
+            throw overflow_error("Oveflow occured during operation");
+        }
         return Complex(
-            real * other.real - imag * other.imag,
-            real * other.imag + imag * other.real
+            real_dif,
+            imag_sum
         );
     }
 
