16a8c90706e86cde69bb71067e5be6459c6c44ef
Daniil Makhov <mahovdnl@gmail.com>
2024-11-21 23:00:18 +0300

    task 5.2 initial commit

---
+++ laba_5/task_2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cstddef>
+#include <string>
+
+using namespace std;
+
+class encoder{
+private:
+    /* data */
+public:
+    encoder(vector<byte> vec){
+
+    }
+    
+    void encode(string filename_in, string filename_out, bool flag) const {
+
+    }
+};
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
