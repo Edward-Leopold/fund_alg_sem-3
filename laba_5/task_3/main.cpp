2faaa749d8a4b90de2c052a12db2fef69d10f9f5
Daniil Makhov <mahovdnl@gmail.com>
2024-11-22 20:50:56 +0300

    logical operations 5.3

--- laba_5/task_3/main.cpp
+++ laba_5/task_3/main.cpp
@@ -10,6 +10,42 @@ public:
         value = val;
     }
 
+    logical_values_array inversion() const{
+        return logical_values_array(~value);
+    }
+
+    logical_values_array conjunction(const logical_values_array& other) const{
+        return logical_values_array(value & other.value);
+    }
+
+    logical_values_array disjunction(const logical_values_array& other) const{
+        return logical_values_array(value | other.value);
+    }
+
+    logical_values_array implication(const logical_values_array& other) const{
+        return logical_values_array(~value | other.value);
+    }
+
+    logical_values_array coimplication(const logical_values_array& other) const{
+        return logical_values_array(value & ~other.value);
+    }
+
+    logical_values_array xor_mod(const logical_values_array& other) const{
+        return logical_values_array(value ^ other.value);
+    }
+
+    logical_values_array equivalence(const logical_values_array& other) const{
+        return logical_values_array(~(value ^ other.value));
+    }
+
+    logical_values_array pierce_arrow(const logical_values_array& other) const{
+        return logical_values_array(~(value | other.value));
+    }
+    
+    logical_values_array sheffer_stroke(const logical_values_array& other) const{
+        return logical_values_array(~(value & other.value));
+    }
+
     unsigned int get_val() const {
         return value;
     }
@@ -18,10 +54,14 @@ public:
 
 
 int main(){
-    logical_values_array ar(3);
-    std::cout << ar.get_val() << "\n";        
+    logical_values_array a1(1);
+    logical_values_array a2(2);
+    logical_values_array a3 = a1.xor_mod(a2);
 
 
+    std::cout << a1.get_val() << "\n";        
+    std::cout << a2.get_val() << "\n";        
+    std::cout << a3.get_val() << "\n";       
 
     return 0;
 }
