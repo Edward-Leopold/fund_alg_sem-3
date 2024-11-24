e9bbf61c68525fe5860307d10b5af54b19fb56ac
Daniil Makhov <mahovdnl@gmail.com>
2024-11-24 20:18:40 +0300

    change type of expirationDate at class PerishableProduct in 5.7

--- laba_5/task_7/main.cpp
+++ laba_5/task_7/main.cpp
@@ -1,43 +1,4 @@
 #include <iostream>
-#include <regex>
-
-struct Date {
-    int day;
-    int month;
-    int year;
-};
-
-int day_of_month(int month, int year){
-    int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    if (year % 4 == 0){
-        days[1] = 29;
-        if (year % 100 == 0 && year % 400 != 0) days[1] = 28;
-    } 
-    return days[month - 1];
-}
-
-Date parseDate(const std::string& dateStr) {
-    // regexp of format "dd.mm.yyyy"
-    std::regex dateRegex(R"((\d{2})\.(\d{2})\.(\d{4}))");
-    std::smatch match;
-
-    if (!std::regex_match(dateStr, match, dateRegex)) {
-        throw std::invalid_argument("Неверный формат даты");
-    }
-
-    int day = std::stoi(match[1].str());
-    int month = std::stoi(match[2].str());
-    int year = std::stoi(match[3].str());
-
-    if ((year > 2050 || year < 1980) || (month < 0 || month > 12)) {
-        throw std::invalid_argument("Неверный формат даты");
-    }
-    if (day < 1 || day > day_of_month(month, year)) {
-        throw std::invalid_argument("Неверный формат даты");
-    }
-
-    return Date{day, month, year};
-}
 
 
 class Product{
@@ -46,10 +7,10 @@ protected:
     unsigned int id;
     double weight;
     double price;
-    int expiration;
+    int period;
 public:
     Product(const std::string &name, unsigned int id, double weight, double price, int storagePeriod)
-            : name(name), id(id), weight(weight), price(price), expiration(storagePeriod) {
+            : name(name), id(id), weight(weight), price(price), period(storagePeriod) {
         if (weight < 0 || price < 0 || storagePeriod < 0) {
             throw std::invalid_argument("Weight, price, and storage period must be non-negative.");
         }
@@ -63,17 +24,19 @@ public:
             "ID: " << id << "\n" <<
             "Weight: " << weight << " kg" << "\n" << 
             "Price: $" << price << "\n" << 
-            "Storage Period: " << expiration << " days" << "\n";
+            "Storage Period: " << period << " days" << "\n";
     }
 };
 
 class PerishableProduct: public Product{
 private:
-    Date expirationDate;
+    int expirationDate;
 public:
-    PerishableProduct(const std::string &name, unsigned int id, double weight, double price, int storagePeriod, std::string expirationDate): 
-    Product(name, id, weight, price, storagePeriod){
-        this->expirationDate = parseDate(expirationDate); 
+    PerishableProduct(const std::string &name, unsigned int id, double weight, double price, int storagePeriod, int expirationDate): 
+    Product(name, id, weight, price, storagePeriod), expirationDate(expirationDate){
+         if (expirationDate < 0){
+            throw std::invalid_argument("Can't store expired product!");
+         }
     }
     double calculateStorageFee() const override{
 
@@ -109,9 +72,17 @@ public:
     Product(name, id, weight, price, storagePeriod), flammability(flammability) {}
 
     double calculateStorageFee() const override{
-        
+
     }
 };
+
+
+class Warehouse{
+    private:
+
+    public:
+
+}; 
 
 
 
