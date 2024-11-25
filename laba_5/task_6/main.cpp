683202229966ab65e5a9369e6c950c6f7692cbab
Daniil Makhov <mahovdnl@gmail.com>
2024-11-25 23:35:55 +0300

    rename fields in class, fix insert function 5.6

--- laba_5/task_6/main.cpp
+++ laba_5/task_6/main.cpp
@@ -4,16 +4,16 @@
 
 class vector{
 private:
-    double* data;
-    std::size_t size;
-    std::size_t capacity;
+    double* v_data;
+    std::size_t v_size;
+    std::size_t v_capacity;
 public:
     vector(const size_t n, const double &init_val = 0.0){
-        size = n;
-        capacity = n;
-        data = new double[n];
+        v_size = n;
+        v_capacity = n;
+        v_data = new double[n];
         for (int i = 0; i < n; i++){
-            data[i] = init_val;
+            v_data[i] = init_val;
         }
     }
 
@@ -21,107 +21,108 @@ public:
         if (start >= end) {
             throw std::invalid_argument("Invalid iterators passed to constructor");
         }
-        size = end - start;
-        capacity = size;
-        data = new double[size];
-        std::copy(start, end, data);
+        v_size = end - start;
+        v_capacity = v_size;
+        v_data = new double[v_size];
+        std::copy(start, end, v_data);
     }
 
     vector(std::vector<double>::const_iterator start, std::vector<double>::const_iterator end){
         if (start >= end) {
             throw std::invalid_argument("Invalid iterators passed to constructor");
         }
-        size = end - start;
-        capacity = size;
-        data = new double[size];
-        std::copy(start, end, data);
+        v_size = end - start;
+        v_capacity = v_size;
+        v_data = new double[v_size];
+        std::copy(start, end, v_data);
     }
 
     vector(std::initializer_list<double> init) {
-        size = init.size();
-        capacity = size;
-        data = new double[size];
-        std::copy(init.begin(), init.end(), data);
+        v_size = init.size();
+        v_capacity = v_size;
+        v_data = new double[v_size];
+        std::copy(init.begin(), init.end(), v_data);
     }
 
     double& at(size_t index) const { // const double& ?????????
-        if (index >= size){
+        if (index >= v_size){
             throw std::invalid_argument("Incorrect index passed");
         }
-        return data[index];
+        return v_data[index];
     }
 
     double& front() const {
-        if (size == 0) {
+        if (v_size == 0) {
             throw std::runtime_error("Can't get front element of empty vector");
         }
-        return data[0];
+        return v_data[0];
     }
 
     double& back() const {
-        if (size == 0) {
+        if (v_size == 0) {
             throw std::runtime_error("Can't get back element of empty vector");
         }
-        return data[size - 1];
+        return v_data[v_size - 1];
     }
 
     double* data() const {
-        return data;
+        return v_data;
     }
 
     bool empty() const {
-        return (size == 0) ? true : false;
+        return (v_size == 0) ? true : false;
     }
 
     size_t size() const{
-        return size;
+        return v_size;
     }
 
     size_t capacity() const{
-        return capacity;
+        return v_capacity;
     }
 
     void reserve(size_t num) {
-        if (num > capacity){
+        if (num > v_capacity){
             double* new_data = new double[num];
             std::memset(new_data, 0, num * sizeof(double));
-            std::memcpy(new_data, data, size * sizeof(double));
-            delete[] data;
-            capacity = num;
-            data = new_data;    
+            std::memcpy(new_data, v_data, v_size * sizeof(double));
+            delete[] v_data;
+            v_capacity = num;
+            v_data = new_data;    
         }
     }
 
     void shrink_to_fit() {
-        if (capacity > size){
-            double* shrinked_data = new double[size];
-            std::memcpy(shrinked_data, data, size * sizeof(double));
-            delete[] data;
-            data = shrinked_data;
-            capacity = size;
+        if (v_capacity > v_size){
+            double* shrinked_data = new double[v_size];
+            std::memcpy(shrinked_data, v_data, v_size * sizeof(double));
+            delete[] v_data;
+            v_data = shrinked_data;
+            v_capacity = v_size;
         }
     }
 
     void clear() {
-        size = 0;
+        v_size = 0;
     }
 
     void insert(size_t index, double elem){
-        if (index > size) {
-            throw std::invalid_argument("Passed index value must be in range of size of vector");
+        if (index > v_size) {
+            throw std::invalid_argument("Passed index value must be in range of v_size of vector");
         }
-        if (capacity < size + 1){
-            reserve(capacity + 100);
+        if (v_capacity == v_size){
+            reserve(v_capacity + 100);
         }
-        for (size_t i = size - 1; i != index; i--){
-            data[i + 1] = data[i];
+
+        v_size += 1;
+        for (size_t i = v_size; i != index; i--){
+            v_data[i] = v_data[i - 1];
         }
-        data[index] = elem;
-        size += 1;
+        v_data[index] = elem;
     } 
 
     ~vector(){
-        delete[] data;
+        delete[] v_data;
     }
     
 };
@@ -130,9 +131,16 @@ public:
 int main(){
     try{
         vector v{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        // double* data = v.capacity();
-        
-        
+        double* data = v.data();
+
+        for (int i = 0; i < v.size(); i++) std::cout << data[i] << " ";
+        std::cout << '\n';
+
+        v.insert(4, 12.9023);
+
+        data = v.data();
+        for (int i = 0; i < v.size(); i++) std::cout << data[i] << " ";
+        std::cout << '\n';
     }
     catch(const std::exception& e){
         std::cerr << e.what() << '\n';
