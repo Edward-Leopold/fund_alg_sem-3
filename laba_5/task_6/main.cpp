72a3e555b7c3e191a722829e7e2eea2b47e5c7c7
Daniil Makhov <mahovdnl@gmail.com>
2024-11-25 23:22:21 +0300

    add some methods for class vector in 5.6

--- laba_5/task_6/main.cpp
+++ laba_5/task_6/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <cstring>
 
 class vector{
 private:
@@ -43,17 +43,96 @@ public:
         data = new double[size];
         std::copy(init.begin(), init.end(), data);
     }
+
+    double& at(size_t index) const { // const double& ?????????
+        if (index >= size){
+            throw std::invalid_argument("Incorrect index passed");
+        }
+        return data[index];
+    }
+
+    double& front() const {
+        if (size == 0) {
+            throw std::runtime_error("Can't get front element of empty vector");
+        }
+        return data[0];
+    }
+
+    double& back() const {
+        if (size == 0) {
+            throw std::runtime_error("Can't get back element of empty vector");
+        }
+        return data[size - 1];
+    }
+
+    double* data() const {
+        return data;
+    }
+
+    bool empty() const {
+        return (size == 0) ? true : false;
+    }
+
+    size_t size() const{
+        return size;
+    }
+
+    size_t capacity() const{
+        return capacity;
+    }
+
+    void reserve(size_t num) {
+        if (num > capacity){
+            double* new_data = new double[num];
+            std::memset(new_data, 0, num * sizeof(double));
+            std::memcpy(new_data, data, size * sizeof(double));
+            delete[] data;
+            capacity = num;
+            data = new_data;    
+        }
+    }
+
+    void shrink_to_fit() {
+        if (capacity > size){
+            double* shrinked_data = new double[size];
+            std::memcpy(shrinked_data, data, size * sizeof(double));
+            delete[] data;
+            data = shrinked_data;
+            capacity = size;
+        }
+    }
+
+    void clear() {
+        size = 0;
+    }
+
+    void insert(size_t index, double elem){
+        if (index > size) {
+            throw std::invalid_argument("Passed index value must be in range of size of vector");
+        }
+        if (capacity < size + 1){
+            reserve(capacity + 100);
+        }
+        for (size_t i = size - 1; i != index; i--){
+            data[i + 1] = data[i];
+        }
+        data[index] = elem;
+        size += 1;
+    } 
+
+    ~vector(){
+        delete[] data;
+    }
     
 };
 
 
 int main(){
     try{
-        std::vector<int> vec ( 3, 3);
-        for (const auto some: vec){
-            std::cout << some << " ";
-        }
-        std::cout << "\n";
+        vector v{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        // double* data = v.capacity();
+        
+        
     }
     catch(const std::exception& e){
         std::cerr << e.what() << '\n';
