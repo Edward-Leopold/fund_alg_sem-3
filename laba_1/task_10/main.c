66260a5ddaf951f026e0d58548c679dcfb043d48
Daniil Makhov <mahovdnl@gmail.com>
2024-12-02 18:34:33 +0300

    done 1.10

--- laba_1/task_10/main.c
+++ laba_1/task_10/main.c
@@ -5,7 +5,11 @@
 #include <math.h>
 
 typedef enum err_codes{
-    INVALID_BASE = 1,
+    SUCCESS,
+    INVALID_BASE,
+    SCANF_ERR,
+    CONVERSION_ERR,
+    MALLOC_ERR,
 } err_codes;
 
 
@@ -23,83 +27,147 @@ int charToInt(char c){
     return -1;
 }
 
-char* gorner(int n, int base){
-    int length = (int)(log(n)/log(base)) + 1;
-    char* result = (char*)malloc(sizeof(char) * (length + 1));
-    if(result == NULL) return NULL;
-    result[length] = '\0';
-    if(n < base){
-       if(n >= 10) result[0] = (n - 10) + 'a';
-       else result[0] = n + '0';
-    }
-    while(n >= base){
-        int div_remain = n % base;
-        if(div_remain < 10){
-            result[--length] = '0' + div_remain;
-        } else{
-            result[--length] = 'a' + (div_remain - 10);
-        }
-        n /= base;
-        if(n < base){
-            if(n >= 10) result[0] = (n - 10) + 'a';
-            else result[0] = n + '0';
-        }
-    }
-    return result;
+char intToChar(int n){
+    if (n >= 0 && n <= 9) return '0' + n;
+    return 'A' + n - 10;
 }
 
-int conversion_to_10(const char *number, int base, long long* result) {
-    long long ans = 0;
-    int d;
-    int sign = 1;
-    for (int i = 0; number[i]; i++) {
-        if (number[i] == '-') {
-            sign = -1;
-            continue;
-        }
-
-        if ('0' <= number[i] && number[i] <= '9') {
-            d = number[i] - '0';
-        } 
-         else if ('A' <= number[i] && number[i] <= 'Z') {
-            d = number[i] - 'A' + 10; 
-        } else {
-            return 1;
-        }
-        if (d >= base) return 1;
-
-        long long t = ans;
-        ans = ans * base;
-        if (ans / base != t) return 1;
-        if (sign == 1 && ans > LONG_MAX - d) return 1;
-        if (sign == -1 && ans < LONG_MIN + d) return 1;
-
-        ans += sign * d;
+void reverseString(char* str, int length) {
+    int i = 0, j = length - 1;
+    while (i < j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
     }
-    *result = ans;
-    return 0;
+}
+
+char* back_gorner(long long num, int base){
+    int capacity = 10;
+    int size = 0;
+    char* res = (char*)malloc(sizeof(char) * capacity);
+    if (!res) return NULL;
+
+    int minus = (num < 0) ? 1 : 0;
+    num = llabs(num);
+
+    if (num == 0) { 
+        res[size++] = '0';
+    }
+
+    while(num != 0){
+        if (size == capacity - 2){
+            capacity *= 2;
+            char* tmp = (char*)realloc(res, capacity);
+            if (!tmp){
+                free(res);
+                return NULL;
+            }
+            res = tmp;
+        }
+        res[size++] = intToChar(num % base);
+        num /= base;
+    }
+    if (minus) {
+        res[size++] = '-';
+    }
+    res[size] = '\0';
+
+    reverseString(res, size);
+    return res;
+}
+
+err_codes conversion_to_10(const char *number, int base, long long* result) {
+    *result = 0;
+    int sign = (number[0] == '-') ? 1 : 0;
+    int digit;
+    for (int i = sign; number[i]; ++i){
+        digit = charToInt(number[i]);
+        if (digit < 0 || digit >= base){
+            return CONVERSION_ERR;
+        }
+        *result = *result * base + digit;
+    }
+    if(sign) {
+        *result *= -1;
+    }
+    return SUCCESS;
+}
+
+char* removeLeadingZeros(char* str) {
+    char* p = str;
+    while (*p == '0') {
+        ++p;
+    }
+
+    if (*p == '\0') {
+        str[0] = '0';
+        str[1] = '\0';
+    } else {
+        memmove(str, p, strlen(p) + 1);
+    }
+
+    return str;
 }
 
 
 int main(int argc, char** argv){
     int base;
-    scanf("%d", &base);
+    printf("Enter base: ");
+    if (scanf("%d", &base) != 1){
+        printf("Error reading base of number!\n");
+        return SCANF_ERR;
+    }
     if(base < 2 || base > 36){
         printf("Invalid base of numbers\n");
         return INVALID_BASE;
     }
 
     char str[256];
-    int max = -INFINITY;
+    long long max = 0;
     char max_str[256];
+    long long cur;
+    int counter = 0;
     while (1) {
-        scanf("%s", str);
+        printf("Enter number in base or \"Stop\" to terminate input: ");
+        scanf("%255s", str);
         if (strcmp(str, "Stop") == 0) {
             break;
         }
+        ++counter;
+
+        err_codes conv_status = conversion_to_10(str, base, &cur);
+        if (conv_status != SUCCESS){
+            printf("Conversion error occured\n");
+            return INVALID_BASE;
+        }
         
+        if (llabs(cur) > llabs(max)){
+            max = cur;
+            removeLeadingZeros(str);
+            strcpy(max_str, str);
+        }
     }
 
+    if (counter == 0){
+        printf("No number was entered. Nothing to convert\n");
+        return 0;
+    }
+
+    printf("Max number by module: %s\n", max_str);
+
+    int bases[4]= {9, 18, 27, 36};
+    for (int i = 0; i < 4; ++i){
+        char* num_str = back_gorner(max, bases[i]);
+        if (num_str == NULL) {
+            printf("Memory allocation error!\n");
+            return MALLOC_ERR;
+        }
+        printf("Max number with base %d: %s\n", bases[i], num_str);
+        free(num_str);
+        num_str = NULL;
+    }
 
 
 
