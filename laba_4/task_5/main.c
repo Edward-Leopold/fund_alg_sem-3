33c69965e3866d5f3dab9f0f6fcb51d203b14f82
Daniil Makhov <mahovdnl@gmail.com>
2024-12-14 00:57:50 +0300

    4.5 initial commit

---
+++ laba_4/task_5/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
+
+
+
+
+
+
+
+
+int main(int argc, char** argv){
+    if (argc < 3){
+        printf("Not enought arguements. Pass flag and files.\n");
+        return 1;
+    }
+
+    return 0;
+}
