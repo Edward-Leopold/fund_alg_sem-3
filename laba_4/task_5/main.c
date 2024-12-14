50d6f4f6a1e8b2eaa3ef4ab61c07c0692dc22cb3
Daniil Makhov <mahovdnl@gmail.com>
2024-12-14 11:58:27 +0300

    some small changes

--- laba_4/task_5/main.c
+++ laba_4/task_5/main.c
@@ -17,6 +17,24 @@ int main(int argc, char** argv){
         printf("Not enought arguements. Pass flag and files.\n");
         return 1;
     }
+    
+    if(strcmp(argv[1], "--calculate") == 0){
+        printf("calc!\n");
+
+        for (int i = 2; i < argc; ++i){
+            FILE* input = fopen(argv[i], "r");
+
+            
+
+            fclose(input);
+        }
+
+    } else if(strcmp(argv[1], "--table") == 0){
+        printf("table!\n");
+    } else{
+        printf("unknown flag!\n");
+        return 1;
+    }
 
     return 0;
 }
